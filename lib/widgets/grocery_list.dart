e362012d9ea07643e1475ce1a7ef6a9fad084c6c
ROSHAN KUMAR YADAV <roshan099999@gmail.com>
2024-09-23 17:41:01 +0530

    adding dismissed the item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -19,15 +19,45 @@ class _GroceryListState extends State<GroceryList> {
       ),
     );
     if (newItemData == null) return;
-     
-     setState(() {
-       _groceryItems.add(newItemData);
-     });
-    
+
+    setState(() {
+      _groceryItems.add(newItemData);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No Items Added yet...'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direcyion) {
+            _removeItem(_groceryItems[index]);
+            
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
@@ -39,17 +69,7 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
-      body: ListView.builder(
-          itemCount: _groceryItems.length,
-          itemBuilder: (ctx, index) => ListTile(
-                title: Text(_groceryItems[index].name),
-                leading: Container(
-                  width: 24,
-                  height: 24,
-                  color: _groceryItems[index].category.color,
-                ),
-                trailing: Text(_groceryItems[index].quantity.toString()),
-              )),
+      body: content,
     );
   }
 }
